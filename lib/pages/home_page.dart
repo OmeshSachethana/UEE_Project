11901529ee70b1a08b96d93bdc5ff8897679272e
OmeshSachethana <sachethana12@gmail.com>
2023-08-11 23:38:00 +0530

    connection successful

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  //sign out user
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Logged In")),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+      body: const Center(child: Text("Logged In")),
     );
   }
 }

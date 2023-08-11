78c773991012d2a075ceb324cffe72e874e67ed2
OmeshSachethana <sachethana12@gmail.com>
2023-08-11 21:37:09 +0530

    checked auth

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:new_app/pages/login_page.dart';
+
+import 'home_page.dart';
 
 //check if the user sign in or not
 class AuthPage extends StatelessWidget {
@@ -6,6 +10,20 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // user is logged in
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+          //user is not logged in
+          else {
+            return LoginPage();
+          }
+        },
+      ),
+    );
   }
 }

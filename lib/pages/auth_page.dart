04b616cc0df86979eefc9f8b2b7d38cf8f1672d4
OmeshSachethana <sachethana12@gmail.com>
2023-08-11 21:26:50 +0530

    firebase auth

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//check if the user sign in or not
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

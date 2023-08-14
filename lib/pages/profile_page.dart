51fcbea59b4b04dacaf9479a02d0132802068d20
OmeshSachethana <sachethana12@gmail.com>
2023-08-15 01:22:04 +0530

    added text_box.dart

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:new_app/components/text_box.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,10 +10,39 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profile Page")),
+      appBar: AppBar(
+        title: const Text("Profile Page"),
+        backgroundColor: Colors.grey[900],
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 50),
+          const Icon(
+            Icons.person,
+            size: 72,
+          ),
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Text(
+              'My Details',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+
+          //username
+          const MyTextBox(text: 'Omesh Sachethana', sectionName: 'username')
+
+          //bio
+
+          //user posts
+        ],
+      ),
     );
   }
 }

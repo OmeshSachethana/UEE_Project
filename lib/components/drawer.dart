e8655404728718c6ede0152005120095799c90d1
OmeshSachethana <sachethana12@gmail.com>
2023-08-15 00:58:34 +0530

    added routes

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/components/my_list_tile.dart';
 
 class MyDrawer extends StatelessWidget {
-  const MyDrawer({super.key});
+  final void Function()? onProfileTap;
+  final void Function()? onSignoutTap;
+
+  const MyDrawer({
+    super.key,
+    required this.onProfileTap,
+    required this.onSignoutTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -9,11 +17,24 @@ class MyDrawer extends StatelessWidget {
       backgroundColor: Colors.grey[900],
       child: Column(children: [
         const DrawerHeader(
-            child: Icon(
-          Icons.person,
-          color: Colors.white,
-          size: 64,
-        )),
+          child: Icon(
+            Icons.person,
+            color: Colors.white,
+            size: 64,
+          ),
+        ),
+        MyListTile(
+          icon: Icons.home,
+          text: 'H O M E',
+          onTap: () => Navigator.pop(context),
+        ),
+
+        //profile
+        MyListTile(
+            icon: Icons.person, text: "P R O F I L E", onTap: onProfileTap),
+
+        MyListTile(
+            icon: Icons.person, text: "L O G O U T", onTap: onSignoutTap),
       ]),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,31 +2,47 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:new_app/components/drawer.dart';
+import 'package:new_app/pages/profile_page.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
+  HomePage({Key? key}) : super(key: key);
 
   final user = FirebaseAuth.instance.currentUser!;
 
-  //sign out user
-  void signUserOut() {
+  // Sign out user
+  void signUserOut(BuildContext context) {
     FirebaseAuth.instance.signOut();
+  }
+
+  void goToProfilePage(BuildContext context) {
+    // Pop the menu drawer
+    Navigator.pop(context);
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ProfilePage(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The wall'),
+        title: const Text('The wall'),
         backgroundColor: Colors.grey[900],
         actions: [
           IconButton(
-            onPressed: signUserOut,
-            icon: Icon(Icons.logout),
+            onPressed: () => signUserOut(context),
+            icon: const Icon(Icons.logout),
           )
         ],
       ),
-      drawer: MyDrawer(),
+      drawer: MyDrawer(
+        onProfileTap: () => goToProfilePage(context),
+        onSignoutTap: () => signUserOut(context),
+      ),
       body: Center(child: Text("Logged In as " + user.email!)),
     );
   }

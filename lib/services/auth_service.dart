e6509b059aaa5b622140e1a33c4ddae5e5fa30f2
OmeshSachethana <sachethana12@gmail.com>
2023-08-14 11:52:04 +0530

    google service done

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,23 @@
-class AuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  // Google Sign In
+  signInWithGoogle() async {
+    //begin with interactive sign in process
+    final GoogleSignInAccount? gUser = await GoogleSignIn()
+        .signIn(); //this opens a new page for select google account
+
+    //obatin auth details from the request
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+
+    //create a new credential for user
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+
+    //finally, lets sigm in
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}

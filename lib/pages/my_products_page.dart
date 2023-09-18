7dbe6641b0e67dfa353e9038f2238a73ab71f4ff
OmeshSachethana <sachethana12@gmail.com>
2023-09-18 16:46:16 +0530

    categorize

--- lib/pages/my_products_page.dart
+++ lib/pages/my_products_page.dart
@@ -41,17 +41,53 @@ class MyProductsPage extends StatelessWidget {
             );
           }
 
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data =
-                  document.data() as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data['name']),
-                subtitle: Text('Category: ${data['category']}'),
-                leading: Image.network(data['image']), // Display image here
-                // Add other product details as needed
+          Map<String, List<DocumentSnapshot>> productsByCategory = {};
+
+          snapshot.data!.docs.forEach((document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            String category = data['category'];
+
+            if (!productsByCategory.containsKey(category)) {
+              productsByCategory[category] = [];
+            }
+
+            productsByCategory[category]!.add(document);
+          });
+
+          return ListView.builder(
+            itemCount: productsByCategory.length,
+            itemBuilder: (BuildContext context, int index) {
+              String category = productsByCategory.keys.elementAt(index);
+
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      category,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: productsByCategory[category]!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      DocumentSnapshot document =
+                          productsByCategory[category]![index];
+                      Map<String, dynamic> data =
+                          document.data() as Map<String, dynamic>;
+
+                      return ListTile(
+                        title: Text(data['name']),
+                        leading:
+                            Image.network(data['image']), // Display image here
+                        // Add other product details as needed
+                      );
+                    },
+                  ),
+                ],
               );
-            }).toList(),
+            },
           );
         },
       ),

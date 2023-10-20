93d1d67c29efb83046d710110cda26e679063ec9
OmeshSachethana <sachethana12@gmail.com>
2023-10-20 07:56:32 +0530

    Added InkWell

--- lib/pages/auction_products_page.dart
+++ lib/pages/auction_products_page.dart
@@ -65,30 +65,29 @@ class AuctionProductsPage extends StatelessWidget {
                     Map<String, dynamic> data =
                         document.data() as Map<String, dynamic>;
 
-                    return Card(
-                      elevation: 5,
-                      margin: const EdgeInsets.all(10.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                                width: 130,
-                                height: 100,
-                                child: Image.network(data['image'])),
-                            Text(data['name']),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        ViewProductPage(document: document),
-                                  ),
-                                );
-                              },
-                              child: Text('View Product'),
-                            ),
-                          ],
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                ViewProductPage(document: document),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 5,
+                        margin: const EdgeInsets.all(10.0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 130,
+                                  height: 100,
+                                  child: Image.network(data['image'])),
+                              Text(data['name']),
+                            ],
+                          ),
                         ),
                       ),
                     );
@@ -125,43 +124,47 @@ class AuctionProductsPage extends StatelessWidget {
 
                 return GridView.builder(
                   shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
+                  physics:
+                      NeverScrollableScrollPhysics(), // to disable GridView's scrolling
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemCount: snapshot.data!.docs.length,
+                      crossAxisCount:
+                          2), // a delegate that allows the caller to specify a number of cross-axis children for a layout that adjusts to fill the viewport in the main axis.
+                  itemCount: snapshot.data!.docs
+                      .length, // the number of products in the database.
                   itemBuilder: (BuildContext context, int index) {
-                    DocumentSnapshot document = snapshot.data!.docs[index];
+                    // builder callback
+                    DocumentSnapshot document =
+                        snapshot.data!.docs[index]; // get document snapshot
                     Map<String, dynamic> data =
-                        document.data() as Map<String, dynamic>;
+                        document.data() as Map<String, dynamic>; // get data
 
-                    return Card(
-                      elevation: 5,
-                      margin: const EdgeInsets.all(10.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                                width: 130,
-                                height: 100,
-                                child: Image.network(data['image'])),
-                            Text(data['name']),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        ViewProductPage(document: document),
-                                  ),
-                                );
-                              },
-                              child: Text('View Product'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () => _startTimer(document),
-                              child: Text('Start Timer'),
-                            ),
-                          ],
+                    return InkWell(
+                      onTap: () {
+                        // handle tap events that occur within this widget.
+                        Navigator.push(
+                          // navigate to a new screen and/or back to the previous screen.
+                          context,
+                          MaterialPageRoute(
+                            // a modal route that replaces the entire screen with a platform-adaptive transition.
+                            builder: (context) => ViewProductPage(
+                                document:
+                                    document), // returns a new widget that will be pushed onto the Navigator stack.
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 5,
+                        margin: const EdgeInsets.all(10.0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 130,
+                                  height: 100,
+                                  child: Image.network(data['image'])),
+                              Text(data['name']),
+                            ],
+                          ),
                         ),
                       ),
                     );
